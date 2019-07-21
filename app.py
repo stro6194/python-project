33d6e87542251571bc263fd20cf2a30520485e22
Jason Baker <jasondbaker@hotmail.com>
2019-07-20 21:13:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def main_page():
-    return 'Welcome to the fancy website!'
+    return render_template('index.html')
 
 @app.route('/health')
 def main_page():
